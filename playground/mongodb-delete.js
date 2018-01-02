8734fe579f9b650b1990898e3194fe2b31f666d3
Peter Bonrad <peter.bonrad@gmail.com>
2018-01-02 13:52:42 +0100

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  const db = client.db('TodoApp');
+
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Users').deleteMany({name: 'Peter'}).then((result) => {
+  //   console.log(result.result);
+  // });
+
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID("5a4b796e5ebabd85a3429a9d")
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // client.close();
+});
